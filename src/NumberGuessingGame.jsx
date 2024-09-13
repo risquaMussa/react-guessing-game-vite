398c1ba9542139e007313c10c104c2645cc30089
risquaMussa <rismussa@gmail.com>
2024-09-13 19:08:55 -0400

    convert class

--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -11,6 +11,52 @@ const MAX_ATTEMPTS = 5;
  *
  * Returns a random integer number from 1-100 inclusive
  */
+
+function NumberGuessingGame() {
+  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
+  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+  const [latestGuess, setLatestGuess] = useState(null);
+
+  const isCorrectGuess = latestGuess === numberToGuess;
+
+  const isGameOver = isCorrectGuess || numberOfGuesses >= MAX_ATTEMPTS;
+
+  //Create a handleInputChange function
+  const handleGuess = (guess) => {
+    setLatestGuess(Number(guess));
+    setNumberOfGuesses((prev) => prev + 1);
+  };
+
+  //create a handleReset function
+  const handleReset = () => {
+    setNumberToGuess(Math.floor(Math.random() * 100) + 1);
+    setNumberOfGuesses(0);
+    setLatestGuess(null);
+  };
+
+  return (
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>
+        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+      </h2>
+      <GuessControl onGuess={handleGuess} />
+
+      {isGameOver ? (
+        <GameOver hasWon={isCorrectGuess} onReset={handleReset} />
+      ) : (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NumberGuessingGame;
+
 //function getRandomNumberOld() {
 //   return Math.floor(Math.random() * 100) + 1;
 // }
@@ -77,47 +123,3 @@ const MAX_ATTEMPTS = 5;
 //     );
 //   }
 // }
-function NumberGuessingGame() {
-  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
-  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
-  const [latestGuess, setLatestGuess] = useState(null);
-
-  const isCorrectGuess = latestGuess === numberToGuess;
-
-  const isGameOver = isCorrectGuess || numberOfGuesses >= MAX_ATTEMPTS;
-
-  //Create a handleInputChange function
-  const handleGuess = (guess) => {
-    setLatestGuess(Number(guess));
-    setNumberOfGuesses((prev) => prev + 1);
-  };
-
-  //create a handleReset function
-  const handleReset = () => {
-    setNumberToGuess(Math.floor(Math.random() * 100) + 1);
-    setNumberOfGuesses(0);
-    setLatestGuess(null);
-  };
-
-  return (
-    <div>
-      <h2>I'm thinking of a number from 1 to 100.</h2>
-      <h2>
-        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
-      </h2>
-      <GuessControl onGuess={handleGuess} />
-
-      {isGameOver ? (
-        <GameOver hasWon={isCorrectGuess} onReset={handleReset} />
-      ) : (
-        <GuessMessage
-          guess={latestGuess}
-          numberToGuess={numberToGuess}
-          numberOfGuesses={numberOfGuesses}
-        />
-      )}
-    </div>
-  );
-}
-
-export default NumberGuessingGame;
